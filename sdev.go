c8582f63a0cee7ec7ef20cb4eb172a798d629a55
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-02-13 11:39:58 -0800

    add go file

---
+++ sdev.go
@@ -0,0 +1,10 @@
+import (
+    "fmt"
+    "github.com/jfrog-solutiontest/employee"
+)
+
+
+func SdevEmployerPackageName () {
+    fmt.Println ("Package Name: Software Developer Employer")
+    employee.sdet()
+}
