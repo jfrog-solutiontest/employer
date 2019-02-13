95970b0ef54c7c37b0d6e1a83c917c19ec2652ff
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-02-13 09:06:39 -0800

    initial

---
+++ sdet.go
@@ -0,0 +1,11 @@
+package employer
+
+import (
+    "fmt"
+    "github.com/jfrog-solutiontest/employee"
+)
+
+func EmployerPackageName () {
+    fmt.Println ("Package Name: Employer")
+    employee.sdet()
+}
