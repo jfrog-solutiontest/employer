eaea79fdedb3f675ce808b4b45f2029e7a82e151
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-02-13 11:26:13 -0800

    add v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/jfrog-solutiontest/employer/v2
+
+require github.com/jfrog-solutiontest/employee v1.0.0 // indirect
---
+++ v2/sdet.go
@@ -0,0 +1,12 @@
+package employer
+
+import (
+    "fmt"
+    "github.com/jfrog-solutiontest/employee"
+)
+
+
+func EmployerPackageName () {
+    fmt.Println ("Package Name: Employer")
+    employee.sdet()
+}
